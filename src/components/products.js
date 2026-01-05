372157b89393800e8bed10484ac989c955a3048b
Rachel Fingerhut <fingerhutrachel@gmail.com>
2026-01-04 17:10:29 -0800

    refactor: Change divs to sections

--- src/components/products.js
+++ src/components/products.js
@@ -1,17 +1,21 @@
 function renderProducts() {
     const content = document.getElementById('content');
 
-    const productsDiv = document.createElement('div');
+    const productsSection = document.createElement('section');
+    productsSection.id = "products";
+
     const title = document.createElement('h2');
+    title.textContent = "Products";
+
     const container = document.createElement('div');
     const productImage = document.createElement('div');
     const productInfo = document.createElement('div');
     productInfo.textContent = 'Our famous Butter Creme Cofee Cake will melt in your mouth.';
 
     container.append(productImage, productInfo);
-    productsDiv.append(title, container);
+    productsSection.append(title, container);
 
-    content.append(productsDiv);
+    content.append(productsSection);
 }
 
 export default renderProducts;
