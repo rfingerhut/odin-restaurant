1e82cddcee5d050d4b48b2db4381de78db3055d4
Rachel Fingerhut <fingerhutrachel@gmail.com>
2026-01-04 16:18:35 -0800

    feat: Add functioning nav bar that switches displayed content in content div

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import "./styles.css";
+import renderHome from "./components/home";
+import renderAbout from "./components/about";
+import renderProducts from "./components/products";
 
-console.log('hello!');
+renderHome();
+
+const content = document.getElementById('content');
+
+const navButtons = document.querySelector(".navButtons");
+
+navButtons.addEventListener('click', (e) => handleNavButtonClick(e));
+
+function handleNavButtonClick(e){
+    const id = e.target.id;
+    content.textContent = '';
+
+    switch (id) {
+        case 'home':
+            renderHome();
+            break;
+
+        case 'about':
+            renderAbout();
+            break;
+
+        case 'products':
+            renderProducts();
+            break;
+    
+        default:
+            break;
+    }
+}
