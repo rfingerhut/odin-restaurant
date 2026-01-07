c353d886a2ed0183dc1032d7fe79c2e327d1ca57
Rachel Fingerhut <fingerhutrachel@gmail.com>
2026-01-06 18:38:43 -0800

    feat: New styles and structure for about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,16 +1,27 @@
+import storefront from '../images/storefront.jpg';
+
+
 function renderAbout () {
     const content = document.getElementById('content');
 
     const aboutSection = document.createElement('section');
-    aboutSection.id = 'about';
+    aboutSection.id = 'aboutSection';
+
+    const textContainer = document.createElement('div');
 
     const h2 = document.createElement('h2');
     h2.textContent = 'Our Story';
 
     const p = document.createElement('p');
-    p.textContent = 'A trip to the Fingerhut Bakery of North Judson, Indiana is like taking a trip back in time. A time when your local neighborhood bakery produced some of the best smells and even better tastes than you can remember.'
+    p.textContent = 'A trip to the Fingerhut Bakery of North Judson, Indiana is like taking a trip back in time. A time when your local neighborhood bakery produced some of the best smells and even better tastes than you can remember.';
 
-    aboutSection.append(h2, p);
+    textContainer.append(h2, p);
+
+    const image = document.createElement('img');
+    image.src = storefront;
+    image.alt = 'Image of Fingerhut Bakery storefront.';
+
+    aboutSection.append(textContainer, image);
     content.append(aboutSection);
 }
 
