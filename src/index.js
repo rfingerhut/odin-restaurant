10b93c328db4ed1df94a7b0eac1baa610088ff68
Rachel Fingerhut <fingerhutrachel@gmail.com>
2026-01-11 12:57:27 -0800

    feat: Add event listeners for logo and shop now buttons

--- src/index.js
+++ src/index.js
@@ -6,13 +6,27 @@ import renderProducts from "./components/products";
 renderHome();
 
 const content = document.getElementById('content');
-
+const shopButton = document.getElementById('shop-now-button');
 const navButtons = document.querySelector(".navButtons");
+const logoButton = document.getElementById('logo');
 
+content.addEventListener('click', (e) => handleShopButtonClick(e));
 navButtons.addEventListener('click', (e) => handleNavButtonClick(e));
+logoButton.addEventListener('click', handleLogoButtonClick);
+
+function handleShopButtonClick(e){
+    const id = e.target.id;
+    if (id === 'shop-now-button'){
+        content.textContent = '';
+        renderProducts();
+    }
+}
 
 function handleNavButtonClick(e){
     const id = e.target.id;
+
+    if(!id) {return;}
+
     content.textContent = '';
 
     switch (id) {
@@ -31,4 +45,9 @@ function handleNavButtonClick(e){
         default:
             break;
     }
+}
+
+function handleLogoButtonClick(){
+    content.textContent = '';
+    renderHome();
 }
